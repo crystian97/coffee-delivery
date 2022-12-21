e7b7d6348dc9160a54d0b4861325649434bd9391
crystian97 <icrystianveloso@gmail.com>
2022-12-20 23:10:15 -0300

    refactor:remove files not necessary

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import { HeaderContainer } from "./styles";
+import Logo from '../../assets/Logo.png'
+export default function Header() {
+    return ( 
+
+        <HeaderContainer>
+            <img src={Logo} alt="" />
+        </HeaderContainer>
+     );
+}
+
