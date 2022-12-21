8be6a14b24a3c92ea4b87b1801cbddfcad570c7e
crystian97 <icrystianveloso@gmail.com>
2022-12-20 23:14:42 -0300

    feat:home component

---
+++ src/pages/Home/Index.tsx
@@ -0,0 +1,16 @@
+import Header from "../../components/Header";
+import { HomeContainer } from "./styles";
+
+function Home() {
+    return (  
+        <>
+         <Header/>
+        <HomeContainer>
+           
+            <h1>Home</h1>
+        </HomeContainer>
+        </>
+    );
+}
+
+export default Home;
