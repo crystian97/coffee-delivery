165fdd4ebbaddeefb4f1233d97defe55d722e2f6
crystian97 <icrystianveloso@gmail.com>
2022-12-20 23:13:58 -0300

    feat:header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+    width: 100%;
+    height: 6.5rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-right: 10rem;
+    padding-left: 10rem;
+`
