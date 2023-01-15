db4da3862faf570fd2c43baad924e4cb33a5bb8d
crystian97 <icrystianveloso@gmail.com>
2023-01-14 23:52:42 -0300

    feat:Items features  finished

--- src/pages/Home/components/Intro/index.tsx
+++ src/pages/Home/components/Intro/index.tsx
@@ -1,13 +1,22 @@
-import { ImageHero, IntroWrapper, Presentation, TextRegularL, Title, TitleXl } from "./styles"
+import { ImageHero, IntroWrapper, Item, ItemsContainer, Presentation, TextRegularL, Title, TitleXl } from "./styles"
 import ImageHeroPng from '../../../../assets/imageHero.png'
+import { Coffee, Package, ShoppingCart, Timer } from "phosphor-react";
+
+
 export default function Intro(){
     return (
         <IntroWrapper>
             <Presentation>
                 <Title>
                 <TitleXl>Encontre o café perfeito  para qualquer hora do dia</TitleXl>
-                <TextRegularL>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</TextRegularL>
+                <TextRegularL>Com o Coffee Delivery você recebe seu café onde estiver, a   qualquer hora</TextRegularL>
                 </Title>
+                <ItemsContainer>
+                    <Item itensColor="yellow_dark"><ShoppingCart size={32} weight="fill"/><p>Compra simples e segura</p></Item>
+                    <Item itensColor="base_text"><Package size={32} weight="fill"/><p>Embalagem mantém o café intacto</p></Item>
+                    <Item itensColor="yellow"><Timer size={32} weight="fill"/><p>Entrega rápida e rastreada</p></Item>
+                    <Item itensColor="purple"><Coffee size={32} weight="fill"/><p>O café chega fresquinho até você</p></Item>
+                </ItemsContainer>
             </Presentation>
             <ImageHero src={ImageHeroPng}/>
         </IntroWrapper>
--- src/pages/Home/components/Intro/styles.ts
+++ src/pages/Home/components/Intro/styles.ts
@@ -17,7 +17,7 @@ export const ImageHero = styled.img`
 export const Presentation = styled.div`
     display: flex;
     flex-direction: column;
-    align-items: flex-start; 
+    justify-content: flex-start;
     height:544px ;
     gap: 16px;
 `;
@@ -43,4 +43,42 @@ export const TextRegularL = styled.p`
     font-size: 20px;
     line-height: 130%;
     color: ${(props) =>props.theme['base-subtitle']};
+`
+export const ItemsContainer = styled.div`
+    width: 35.438rem;
+    height: 5.25rem;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px 40px;
+    
+`;
+const ITEMS_PROPS = {
+yellow:'yellow',
+yellow_dark:'yellow-dark',
+purple:'purple',
+base_text:'base-text'
+
+
+} as const
+
+interface Items{
+    itensColor:keyof typeof ITEMS_PROPS
+}
+export const Item = styled.div<Items>`
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 130%;
+    display: flex;
+    align-items: center;
+    gap: 12px;
+    height: 32px;
+    font-weight: 400;
+    color: ${(props)=>props.theme['base-text']};
+    svg{
+        border-radius: 50%;
+        padding: 8px;
+        background-color: ${(props)=>props.theme[ITEMS_PROPS[props.itensColor]]};
+        color: ${(prosp) =>prosp.theme['white']};
+    }
+
 `
